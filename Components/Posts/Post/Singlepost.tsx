52e53cbfffc41d9fde61477e12b5adb78e4d8bbb
Pardeep Singh <89510434+Pardeeep@users.noreply.github.com>
2021-08-26 16:27:47 +0530

    Update Singlepost.tsx

--- Components/Posts/Post/Singlepost.tsx
+++ Components/Posts/Post/Singlepost.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useRouter } from 'next/router'
+import Link from 'next/link'
 import styles from '../../../styles/Home.module.css'
 import moment from 'moment'
 import { Post } from '../../../models/Post'
@@ -12,7 +12,7 @@ function Singlepost({data} : PostDetailViewProps) {
     const router = useRouter();
   return (
     <div className={styles.container}>
-      <button className={styles.back} onClick={() => router.back()}>
+      <button className={styles.back} href="/">
         Back
       </button>
       <h1 className={styles.title}>{data.Title}</h1>
